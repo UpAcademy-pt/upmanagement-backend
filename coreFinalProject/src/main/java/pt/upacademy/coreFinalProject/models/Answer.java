dfaf67f6f0ad11ed87546562f107fc338aa645e2
Jorge Antunes <jmpdantunes@gmail.com>
2020-01-16 17:22:26 +0000

    Modelo Answer, question e questionnaire

---
+++ coreFinalProject/src/main/java/pt/upacademy/coreFinalProject/models/Answer.java
@@ -0,0 +1,19 @@
+package pt.upacademy.coreFinalProject.models;
+
+public class Answer {
+
+	private enum AnswerType {
+		multiple,
+		open,
+		singleOption
+	}
+	
+	private String answer;
+	private AnswerType rtype;
+	private long questionId;
+	
+	
+	
+	
+	
+}
---
+++ coreFinalProject/src/main/java/pt/upacademy/coreFinalProject/models/Questionnaire.java
@@ -0,0 +1,35 @@
+package pt.upacademy.coreFinalProject.models;
+
+import java.util.List;
+
+public class Questionnaire {
+
+	private List<Question> questionList;
+	private String name;
+	private boolean template;
+	
+	public List<Question> getQuestionList() {
+		return questionList;
+	}
+	public void setQuestionList(List<Question> questionList) {
+		this.questionList = questionList;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isTemplate() {
+		return template;
+	}
+	public void setTemplate(boolean template) {
+		this.template = template;
+	}
+	
+	
+	
+	
+	
+	
+}
