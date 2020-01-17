982eae628e30b5a081f8b25d5335713e165499c0
rute-m-t <rute.m.t@gmail.com>
2020-01-17 14:28:58 +0000

    added model account (without connections to other entities) and accountDTO

---
+++ src/main/java/pt/upacademy/coreFinalProject/models/DTOS/AccountDTO.java
@@ -0,0 +1,176 @@
+package pt.upacademy.coreFinalProject.models.DTOS;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDTO extends EntityDTO {
+	
+	private long userId;
+	private int age;
+	private List<Long> academies = new ArrayList<Long>();
+	private String academicDegree;
+	private String academicBackgroung;
+	private String photoLink;
+	private int mobilePhone;
+	private String linkedInAdress;
+	//themes so vai ter valores para formadores
+	private List<String> themes = new ArrayList<String>();
+	//evaluations so vai ter valores para formandos
+	private List<String> evaluations= new ArrayList<String>();
+	private List<LocalDate> missedDays = new ArrayList<LocalDate>();
+	
+	
+	
+	public AccountDTO() {}
+
+
+
+	public AccountDTO(long userId, int age, String academicDegree, String academicBackgroung, String photoLink,
+			int mobilePhone, String linkedInAdress) {
+		this.userId = userId;
+		this.age = age;
+		this.academicDegree = academicDegree;
+		this.academicBackgroung = academicBackgroung;
+		this.photoLink = photoLink;
+		this.mobilePhone = mobilePhone;
+		this.linkedInAdress = linkedInAdress;
+	}
+
+
+
+	public long getUserId() {
+		return userId;
+	}
+
+
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public List<Long> getAcademies() {
+		return academies;
+	}
+
+
+
+	public void setAcademies(List<Long> academies) {
+		this.academies = academies;
+	}
+
+
+
+	public String getAcademicDegree() {
+		return academicDegree;
+	}
+
+
+
+	public void setAcademicDegree(String academicDegree) {
+		this.academicDegree = academicDegree;
+	}
+
+
+
+	public String getAcademicBackgroung() {
+		return academicBackgroung;
+	}
+
+
+
+	public void setAcademicBackgroung(String academicBackgroung) {
+		this.academicBackgroung = academicBackgroung;
+	}
+
+
+
+	public String getPhotoLink() {
+		return photoLink;
+	}
+
+
+
+	public void setPhotoLink(String photoLink) {
+		this.photoLink = photoLink;
+	}
+
+
+
+	public int getMobilePhone() {
+		return mobilePhone;
+	}
+
+
+
+	public void setMobilePhone(int mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+
+
+	public String getLinkedInAdress() {
+		return linkedInAdress;
+	}
+
+
+
+	public void setLinkedInAdress(String linkedInAdress) {
+		this.linkedInAdress = linkedInAdress;
+	}
+
+
+
+	public List<String> getThemes() {
+		return themes;
+	}
+
+
+
+	public void setThemes(List<String> themes) {
+		this.themes = themes;
+	}
+
+
+
+	public List<String> getEvaluations() {
+		return evaluations;
+	}
+
+
+
+	public void setEvaluations(List<String> evaluations) {
+		this.evaluations = evaluations;
+	}
+
+
+
+	public List<LocalDate> getMissedDays() {
+		return missedDays;
+	}
+
+
+
+	public void setMissedDays(List<LocalDate> missedDays) {
+		this.missedDays = missedDays;
+	}
+	
+	
+	
+	
+
+}
