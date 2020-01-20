592609805a0d17e357a64a7414c1bf2db79b7712
rute-m-t <rute.m.t@gmail.com>
2020-01-20 03:23:25 +0000

    account controller tested

--- src/main/java/pt/upacademy/coreFinalProject/controllers/AccountController.java
+++ src/main/java/pt/upacademy/coreFinalProject/controllers/AccountController.java
@@ -1,15 +1,7 @@
 package pt.upacademy.coreFinalProject.controllers;
 
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import pt.upacademy.coreFinalProject.models.Account;
 import pt.upacademy.coreFinalProject.models.DTOS.AccountDTO;
@@ -21,37 +13,4 @@ import pt.upacademy.coreFinalProject.services.AccountService;
 @RequestScoped
 public class AccountController extends EntityControllerDTO<AccountService,AccountRepository,AccountConverter,Account,AccountDTO> {
 
-	@Override
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String create(AccountDTO account) {
-		System.out.println("Sucesso!");
-		return "Success Again!";
-	}
-	
-	@Override
-	@GET
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public AccountDTO get(@PathParam("id") long id) {
-		return new AccountDTO(1, 30, null,"MSc","Biochem","www",1234,"www",null,null,null);
-	}
-	
-	@Override
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String update(AccountDTO user) {
-		System.out.println("Update funciona");
-		return "Update Done!";
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String delete(@PathParam("id") long id) {
-		System.out.println("Delete funciona");
-		return "Delete Done!";
-	}
 }
