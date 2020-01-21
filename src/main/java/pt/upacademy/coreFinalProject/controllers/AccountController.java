3cf0e9316998065d14787cea40ef122d5728dae5
Sofia <lucas.a.sofia@gmail.com>
2020-01-21 20:08:19 +0000

    fix path no AccountController (getAccountByUserId)

--- src/main/java/pt/upacademy/coreFinalProject/controllers/AccountController.java
+++ src/main/java/pt/upacademy/coreFinalProject/controllers/AccountController.java
@@ -3,22 +3,18 @@ package pt.upacademy.coreFinalProject.controllers;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import pt.upacademy.coreFinalProject.models.Account;
-import pt.upacademy.coreFinalProject.models.User;
 import pt.upacademy.coreFinalProject.models.DTOS.AccountDTO;
 import pt.upacademy.coreFinalProject.models.converters.AccountConverter;
-import pt.upacademy.coreFinalProject.models.converters.UserConverter;
 import pt.upacademy.coreFinalProject.repositories.AccountRepository;
 import pt.upacademy.coreFinalProject.services.AccountService;
-import pt.upacademy.coreFinalProject.services.UserService;
+
 
 
 @RequestScoped
@@ -30,7 +26,7 @@ public class AccountController extends EntityControllerDTO <AccountService, Acco
 	
 	
 	@GET
-	@Path("/{id}")
+	@Path("/user/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public AccountDTO getAccountByUserById (@PathParam("id") long userId) {
 		Account account = service.getAccountByUserById(userId);
