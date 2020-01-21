20130762b839ebc9dbd0e2f87f517c39f978043b
martinalferreira <martina.lopes19@gmail.com>
2020-01-21 10:37:44 +0000

    added attributes status to Academy and nif to Account

--- src/main/java/pt/upacademy/coreFinalProject/models/Academy.java
+++ src/main/java/pt/upacademy/coreFinalProject/models/Academy.java
@@ -18,11 +18,12 @@ public class Academy extends EntityRoot {
     private List<Module> modules;
     @ManyToMany
     private List<Account> students;
+    private Status status;
     
     public Academy() {}
     
     public Academy(String client, LocalDate startDate, LocalDate endDate, String edName, 
-		List<Module> modules, List<Account> students) {
+		List<Module> modules, List<Account> students, Status status) {
 
 		this.setClient(client);
 		this.setStartDate(startDate);
@@ -30,8 +31,17 @@ public class Academy extends EntityRoot {
 		this.setEdName(edName);
 		this.setModules(modules);
 		this.setStudents(students);
+		this.setStatus(status);
     }
     
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 
 	public String getClient() {
 		return client;
