4f8b1237043ec5605e44a0dff47ea2c310775a68
Sofia <lucas.a.sofia@gmail.com>
2020-01-24 18:55:09 +0000

    add Class Subscription to avoid @ManyToMany

--- src/main/java/pt/upacademy/coreFinalProject/models/Account.java
+++ src/main/java/pt/upacademy/coreFinalProject/models/Account.java
@@ -1,52 +1,59 @@
 package pt.upacademy.coreFinalProject.models;
 
-import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-
+import javax.persistence.OneToMany;
 
 @Entity
-@NamedQueries({
-	@NamedQuery(name = Account.GET_ALL_ACCOUNTS, query = "SELECT a FROM Account a"),
-	@NamedQuery(name = Account.GET_ALL_ACCOUNT_IDs, query = "SELECT a.id FROM Account a"),
-	@NamedQuery(name = Account.GET_ACCOUNT_COUNT, query = "SELECT COUNT(a.id) FROM Account a"), 
-	@NamedQuery (name = Account.GET_ACCOUNT_BY_USERID, query = "SELECT a FROM Account a WHERE a.userId = :userId")})
+@NamedQueries({ @NamedQuery(name = Account.GET_ALL_ACCOUNTS, query = "SELECT a FROM Account a"),
+		@NamedQuery(name = Account.GET_ALL_ACCOUNT_IDs, query = "SELECT a.id FROM Account a"),
+		@NamedQuery(name = Account.GET_ACCOUNT_COUNT, query = "SELECT COUNT(a.id) FROM Account a"),
+		@NamedQuery(name = Account.GET_ACCOUNT_BY_USERID, query = "SELECT a FROM Account a WHERE a.userId = :userId") })
 
+public class Account extends EntityRoot {
 
-public class Account extends EntityRoot  {
-	
 	private static final long serialVersionUID = 1L;
-
 
 	public static final String GET_ALL_ACCOUNTS = "getAllAccounts";
 	public static final String GET_ALL_ACCOUNT_IDs = "getAllAccountsIds";
 	public static final String GET_ACCOUNT_COUNT = "getAccountCount";
 	public static final String GET_ACCOUNT_BY_USERID = "getAccountByUserId";
-	
+
 	private long userId;
-	
+	@OneToMany(mappedBy = "account")
+	private Set<Subscription> subscriptions;
+
 	public Account() {
 	}
-	
-	public Account(long userId) {
+
+	public Account(long userId, Set<Subscription> subscriptions) {
+		super();
 		this.userId = userId;
+		this.subscriptions = subscriptions;
 	}
-	
 
 	public long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(long userId) {
 		this.userId = userId;
 	}
 
-	
+	public Set<Subscription> getSubscriptions() {
+		return subscriptions;
+	}
+
+	public void setSubscriptions(Set<Subscription> subscriptions) {
+		this.subscriptions = subscriptions;
+	}
+
 	@Override
 	public String toString() {
-		return "Account [userId=" + userId + "]";
+		return "Account [userId=" + userId + "subscriptions=" + subscriptions + "]";
 	}
-	
 
 }
