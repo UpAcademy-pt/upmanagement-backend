99695fe4d96f4772a7d613a86b715244d6d0a75e
CristianoVale <cristiano.fcvale@gmail.com>
2020-01-29 17:50:07 +0000

    Fixed DeclarationsController e COnverter

--- src/main/java/pt/upacademy/coreFinalProject/controllers/academyManager/DeclarationsController.java
+++ src/main/java/pt/upacademy/coreFinalProject/controllers/academyManager/DeclarationsController.java
@@ -7,6 +7,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.Response;
 import pt.upacademy.coreFinalProject.controllers.EntityControllerDTO;
 import pt.upacademy.coreFinalProject.models.Declarations;
 import pt.upacademy.coreFinalProject.models.DTOS.DeclarationsDTO;
+import pt.upacademy.coreFinalProject.models.DTOS.MissedClassDTO;
 import pt.upacademy.coreFinalProject.models.converters.DeclarationsConverter;
 import pt.upacademy.coreFinalProject.repositories.DeclarationsRepository;
 import pt.upacademy.coreFinalProject.services.DeclarationsService;
@@ -43,5 +45,14 @@ public class DeclarationsController extends EntityControllerDTO<DeclarationsServ
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<DeclarationsDTO> getallbyReceiverId(@PathParam("id") int accountId) {
 		return service.getAllDeclarationsByReceiverId(accountId).stream().map(E -> converter.toDTO(E)).collect(Collectors.toList());
+	}
+	
+	@PUT
+	@Path("/verify")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String update(DeclarationsDTO mclass) {
+		service.update(converter.toEntity(mclass));
+		return "Update Done!";
 	}
 }
